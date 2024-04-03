f2a55ed392307bee4646f52a051de9acfc1c6565
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2024-04-03 08:36:11 -0300

    style: Create the button component

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
+
+const button = tv({
+  base: 'border border-white/10 rounded-md p-1.5',
+  variants: {
+    variant: {
+      primary: 'bg-white/10 ',
+      outline: 'bg-black/20',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function IconButton({ variant, ...props }: ButtonProps) {
+  return <button className={button({ variant })} {...props} />
+}
