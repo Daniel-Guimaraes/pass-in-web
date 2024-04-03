00a3ae23cf2d1606bbf9df519c4e05b47a1e0ee1
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2024-04-03 15:28:19 -0300

    feat: Installing and configuring eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/header";
-import { AttendeeList } from "./components/attendee-list";
+import { Header } from './components/header'
+import { AttendeeList } from './components/attendee-list'
 
 export function App() {
   return (
@@ -9,5 +9,3 @@ export function App() {
     </div>
   )
 }
-
-
